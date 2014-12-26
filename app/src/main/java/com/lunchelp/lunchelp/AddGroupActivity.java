fc6740431843e09d8799f3a1826a540c06fc72c5
Mitchell Bruckner <mbruckner807@gmail.com>
2014-12-26 15:32:44 -0600

    initial change commit, start menu code

--- app/src/main/java/com/lunchelp/lunchelp/AddGroupActivity.java
+++ app/src/main/java/com/lunchelp/lunchelp/AddGroupActivity.java
@@ -1,38 +1,24 @@
 package com.lunchelp.lunchelp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import java.util.ArrayList;
 
 
-public class MainScreen extends ActionBarActivity {
+public class AddGroupActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
-        ListView home_list = (ListView) findViewById(R.id.home_list);
-        ArrayList<String> test_array = new ArrayList<>();
-        test_array.add("HELLO");
-        test_array.add("GOODBYE");
-        for(int i=0;i<20;i++){
-            test_array.add(""+i);
-        }
-        ArrayAdapter<String> test_adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,test_array);
-        home_list.setAdapter(test_adapter);
+        setContentView(R.layout.activity_add_group);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_add_group, menu);
         return true;
     }
 
@@ -49,10 +35,5 @@ public class MainScreen extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void add_group(View view){
-        Intent intent = new Intent(this,AddGroupActivity.class);
-        startActivity(intent);
     }
 }
